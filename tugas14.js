984746abd9ce9b2c722a4108a84f101593e7ebd1
jauhhari <ervanadiwijaya@gmail.com>
2021-07-01 08:37:17 +0700

    ok

---
+++ tugas14.js
@@ -0,0 +1,13 @@
+var a = 37;
+var b = 25;
+
+function pembagian(x, y) {
+    var hasil = x / y;
+    return hasil;
+}
+
+console.log(pembagian(a, b))
+console.log(Math.ceil(pembagian(a, b)));
+console.log(Math.floor(pembagian(a, b)));
+console.log(Math.round(pembagian(a, b)));
+
